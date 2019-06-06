b105ea5dce41328a3ca381a8df993a8219d152b5
Christopher Winand <cwinand.6@gmail.com>
2019-06-06 13:53:37 -0400

    Redirect to profile after confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   get '/profile', to: 'users#show'
   get '/users/:id', to: 'users#show'
 
