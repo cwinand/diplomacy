c46792471688d09e3fde882b3ba4b9cb29818c44
Winand, Christopher <cwinand.6@gmail.com>
2019-09-26 16:45:03 -0400

    Add validity for move orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,11 +5,43 @@ class Order < ApplicationRecord
   belongs_to :unit
 
   def valid?
-    self.order_type == 'move' && valid_move?
+    case self.order_type
+    when 'move'
+      return valid_move?
+    when 'support'
+      return valid_support?
+    end
   end
 
   def valid_move?
-    ProvinceBorder.where(province_code: self.start, border_province_code: self.end).count != 0
+    border = ProvinceBorder.find_by(province_code: self.start, border_province_code: self.end)
+
+    if !border
+      return false
+    end
+
+    if self.unit.unit_type == 'f'
+
+      # Fleet moves to a split coast need to use the correct coast
+      if border.border_coastal_code && border.border_coastal_code != self.end_coast
+        return false
+      end
+
+      # Fleets can't move to land
+      if Province.find_by(province_code: self.end).province_type == 'land'
+        return false
+      end
+
+    elsif self.unit.unit_type == 'a'
+
+      # Armies can't move to sea
+      if Province.find_by(province_code: self.end).province_type == 'sea'
+        return false
+      end
+
+    end
+
+    return true
   end
 
 
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -2,12 +2,24 @@ require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
 
-  test 'valid move is valid' do
-    assert orders(:valid_move).valid?
+  test 'move to bordered province is valid' do
+    assert orders(:a_move_par_bur).valid?
+    assert orders(:a_move_con_bul).valid?
+    assert orders(:f_move_aeg_eas).valid?
   end
 
-  test 'invalid move is invalid' do
-    assert_not orders(:invalid_move).valid?
+  test 'move fleet to coast/correct split coast is valid' do
+    assert orders(:f_move_aeg_con).valid?
+    assert orders(:f_move_aeg_bul_sc).valid?
+  end
+
+  test 'move to unbordered province is invalid' do
+    assert_not orders(:a_move_par_bel).valid?
+  end
+
+  test 'move fleet to wrong split coast is invalid' do
+    assert_not orders(:f_move_aeg_bul_nil).valid?
+    assert_not orders(:f_move_aeg_bul_ec).valid?
   end
 
 end
