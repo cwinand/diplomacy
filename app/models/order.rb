89df6745e6d07f4d27ec550a9eb27e9b56aab5e0
Winand, Christopher <cwinand.6@gmail.com>
2019-10-23 11:52:19 -0400

    Update tests to use fewer fixtures, fix null/nil fixture issue, finish order legal tests

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,6 +10,8 @@ class Order < ApplicationRecord
     when 'support'
       return legal_support?
     end
+
+    return false
   end
 
   def legal_move?
@@ -22,7 +24,9 @@ class Order < ApplicationRecord
     if self.unit.unit_type == 'f'
 
       # Fleet moves to a split coast need to use the correct coast
-      if border.border_coastal_code && border.border_coastal_code != self.end_coast
+      if border.border_coastal_code &&
+          border.border_coastal_code != self.end_coast &&
+          border.border_coastal_code != 'both'
         return false
       end
 
@@ -44,27 +48,25 @@ class Order < ApplicationRecord
   end
 
   def legal_support?
-    if self.support_order_type == 'move'
-      # Don't need to match support's coast, just if it has a border, so use the border's coast value
-      border_coast = nil
-      if self.unit.unit_type == 'f'
-        border_coast = ProvinceBorder
-          .find_by(province_code: self.support_start, border_province_code: self.support_end)
-          .border_coastal_code
-      end
+    # Don't need to match support's coast, just if it has a border, so use the border's coast value
+    border_coast = nil
+    if self.unit.unit_type == 'f'
+      border_coast = ProvinceBorder
+        .find_by(province_code: self.support_start, border_province_code: self.support_end)
+        .border_coastal_code
+    end
 
-      # Would this support be a valid move (excluding split coasts, they don't affect this check)
-      legal_move = Order.new(
-        start: self.support_start,
-        end: self.support_end,
-        end_coast: border_coast,
-        order_type: 'move',
-        unit: Unit.new(unit_type: self.unit.unit_type)
-      )
+    # Would this support be a valid move (excluding split coasts, they don't affect this check)
+    legal_move = Order.new(
+      start: self.start,
+      end: self.support_end,
+      end_coast: border_coast,
+      order_type: 'move',
+      unit: Unit.new(unit_type: self.unit.unit_type)
+    )
 
-      if !legal_move.legal?
-        return false
-      end
+    if !legal_move.legal?
+      return false
     end
 
     # Is there an order corresponding to this support in its current turn
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,43 +3,245 @@ require 'test_helper'
 class OrderTest < ActiveSupport::TestCase
 
   test 'move to bordered province is legal' do
-    assert orders(:a_move_par_bur).legal?
-    assert orders(:a_move_con_bul).legal?
-    assert orders(:f_move_aeg_eas).legal?
+    orders = Order.create([
+      {
+        start: 'par',
+        end: 'bur',
+        order_type: 'move',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_fr),
+        unit: units(:fr_army_par)
+      },
+      {
+        start: 'con',
+        end: 'bul',
+        order_type: 'move',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_tr),
+        unit: units(:tr_army_con)
+      },
+      {
+        start: 'eas',
+        end: 'aeg',
+        order_type: 'move',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_tr),
+        unit: units(:tr_fleet_eas)
+      }
+    ])
+
+    assert orders[0].legal?
+    assert orders[1].legal?
+    assert orders[2].legal?
   end
 
   test 'move fleet to coast/correct split coast is legal' do
-    assert orders(:f_move_aeg_con).legal?
-    assert orders(:f_move_aeg_bul_sc).legal?
+    order = Order.create(
+      start: 'aeg',
+      end: 'con',
+      order_type: 'move',
+      turn: turns(:first),
+      game_country: game_countries(:game_country_tr),
+      unit: units(:tr_fleet_aeg)
+    )
+
+    assert order.legal?
+
+    order.update(
+      end: 'bul',
+      end_coast: 'sc'
+    )
+
+    assert order.legal?
+  end
+
+  test 'a fleet in portugal can move to either spanish coast' do
+    order = Order.create(
+      start: 'por',
+      end: 'spa',
+      end_coast: 'nc',
+      order_type: 'move',
+      turn: turns(:first),
+      game_country: game_countries(:game_country_it),
+      unit: units(:it_fleet_por)
+    )
+
+    assert order.legal?
+
+    order.update(end_coast: 'sc')
+
+    assert order.legal?
   end
 
   test 'move to unbordered province is illegal' do
-    assert_not orders(:a_move_par_bel).legal?
+    order = Order.create(
+      start: 'rom',
+      end: 'pie',
+      order_type: 'move',
+      turn: turns(:first),
+      game_country: game_countries(:game_country_it),
+      unit: units(:it_army_rom)
+    )
+
+    assert_not order.legal?
   end
 
   test 'move fleet to wrong split coast is illegal' do
-    assert_not orders(:f_move_aeg_bul_nil).legal?
-    assert_not orders(:f_move_aeg_bul_ec).legal?
+    order = Order.create(
+      start: 'wes',
+      end: 'spa',
+      order_type: 'move',
+      turn: turns(:first),
+      game_country: game_countries(:game_country_it),
+      unit: units(:it_fleet_wes)
+    )
+
+    assert_not order.legal?
+
+    order.update( end_coast: 'nc' )
+
+    assert_not order.legal?
   end
 
   test 'correct support of move is legal' do
-    assert orders(:f_aeg_support_a_con_bul).legal?
+    orders = Order.create([
+      {
+        start: 'lon',
+        end: 'wal',
+        order_type: 'move',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_en),
+        unit: units(:en_army_lon)
+      },
+      {
+        start: 'eng',
+        end: 'eng',
+        order_type: 'support',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_en),
+        unit: units(:fr_fleet_eng),
+        support_start: 'lon',
+        support_end: 'wal',
+        support_order_type: 'move',
+        support_order_unit_type: 'a'
+      }
+    ])
+
+    assert orders[0].legal?
+    assert orders[1].legal?
   end
 
   test 'support for a move that is not performed is not legal' do
-    assert_not orders(:a_ber_support_a_mun_hold).legal?
+    orders = Order.create([
+      {
+        start: 'mun',
+        end: 'sil',
+        order_type: 'move',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_de),
+        unit: units(:de_army_mun)
+      },
+      {
+        start: 'ber',
+        end: 'ber',
+        order_type: 'support',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_de),
+        unit: units(:de_army_ber),
+        support_start: 'mun',
+        support_end: 'mun',
+        support_order_type: 'hold',
+        support_order_unit_type: 'a'
+      }
+    ])
+
+    assert_not orders[1].legal?
   end
 
   test 'support for a supporting unit as a hold is legal' do
-    assert orders(:f_eng_support_f_pic_hold).legal?
+    orders = Order.create([
+      {
+        start: 'spa',
+        end: 'spa',
+        order_type: 'support',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_fr),
+        unit: units(:fr_army_spa),
+        support_start: 'por',
+        support_end: 'por',
+        support_order_type: 'hold',
+        support_order_unit_type: 'a'
+      },
+      {
+        start: 'por',
+        end: 'por',
+        order_type: 'support',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_fr),
+        unit: units(:fr_army_por),
+        support_start: 'spa',
+        support_end: 'spa',
+        support_order_type: 'hold',
+        support_order_unit_type: 'a'
+      }
+    ])
+
+    assert orders[0].legal?
+    assert orders[1].legal?
   end
 
   test 'support from an army to fleet in sea is illegal' do
-    assert_not orders(:a_con_support_f_aeg_eas).legal?
+    orders = Order.create([
+      {
+        start: 'yor',
+        end: 'yor',
+        order_type: 'support',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_en),
+        unit: units(:en_army_yor),
+        support_start: 'nth',
+        support_end: 'nth',
+        support_order_type: 'hold',
+        support_order_unit_type: 'f'
+      },
+      {
+        start: 'nth',
+        end: 'nth',
+        order_type: 'hold',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_en),
+        unit: units(:en_fleet_nth)
+      }
+    ])
+
+    assert_not orders[0].legal?
   end
 
   test 'support from a fleet to army on land is illegal' do
-    assert_not orders(:f_bre_support_a_pic_par).legal?
+    orders = Order.create([
+      {
+        start: 'pic',
+        end: 'par',
+        order_type: 'move',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_fr),
+        unit: units(:fr_army_pic)
+      },
+      {
+        start: 'bre',
+        end: 'bre',
+        order_type: 'support',
+        turn: turns(:first),
+        game_country: game_countries(:game_country_fr),
+        unit: units(:fr_fleet_bre),
+        support_start: 'pic',
+        support_end: 'par',
+        support_order_type: 'move',
+        support_order_unit_type: 'a'
+      }
+    ])
+
+    assert_not orders[1].legal?
   end
 
 end
